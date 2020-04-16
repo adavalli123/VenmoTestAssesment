77b34578fb4f725a3c41b782bedcb755085fae05
srikanth_iOS <adavalli123@gmail.com>
2020-04-15 21:48:48 -0500

    Delete MockUIApplicationTest.swift

--- VenmoAssesmentUITests/MockUIApplicationTest.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MockUIApplicationTest.swift
-//  VenmoAssesmentUITests
-//
-//  Created by Varshini Thatiparthi on 4/15/20.
-//  Copyright © 2020 Srikanth Adavalli. All rights reserved.
-//
-
-import Foundation
